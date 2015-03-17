d679ebf8ab8972b2e3319524af8a76cde7d4194c
Omar Gammoh <omar.gammoh@gmail.com>
2015-03-17 21:17:37 +0100

    update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/tmux source-file
 
-echo "----------------"
-running pylog485/start.sh
+echo "-------WEILCOME-TO-PYLOG485-------"
+echo "->this software was developed by omar gammoh (omar.gammoh@gmail.com)"
+echo "->making sure the server is running in a tmuw session: sudo python /home/pi/pylog485/manage.py runserver 0.0.0.0:9001"
 (tmux ls) || (tmux new-session -d; tmux split-window -d -t 0 -h; tmux send-keys -t 0 'sudo python /home/pi/pylog485/manage.py runserver 0.0.0.0:9001' enter C-l)
-echo "----------------"
+echo "->to attach the session: tmux a -t 0"
+echo "->to detach the session type: ctrl-b then d"
+echo "->to kill the session: tmux kill-session -t 0"
+echo "->to run the session: . /home/pi/pylog485/start.sh"
+echo "---------------ENJOY--------------"
